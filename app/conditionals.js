9ac3c283b582473475f2b668917db2c413dbb58b
TrevorDouty <tdouty@nnu.edu>
2020-10-05 20:48:32 -0600

    switches

--- app/conditionals.js
+++ app/conditionals.js
@@ -1,8 +1,7 @@
 //1. Given two numbers, write a function that will return  the larger number
 
 function largerNum(num1, num2) {
-    // you code here
-
+    return Math.max(num1, num2)
 }
 
 // 2. Given two numbers, amount correct and amount possible of a test, return the corresponding letter grade.
@@ -14,6 +13,24 @@ function largerNum(num1, num2) {
 // output: "F"
 
 function testGrader(score, possible) {
+
+    if (score / possible < .6) {
+        return "F"
+    }
+    else if (score / possible >= .6 && score / possible < .7) {
+        return "D"
+    }
+    else if (score / possible >= .70 && score / possible < .80) {
+        return "C"
+    }
+    else if (score / possible >= .80 && score / possible < .90) {
+        return "B"
+    }
+    else if (score / possible >= .90) {
+        return "A"
+    }
+
+
 
 }
 
@@ -30,12 +47,35 @@ function testGrader(score, possible) {
 
 function timeOfDayGreeting(hour) {
     // you code below
+    if (hour >= 1 && hour <= 4) {
+        return "good night"
+    }
+    else if (hour >= 5 && hour <= 11) {
+        return "good morning"
+    }
+    else if (hour >= 12 && hour <= 17) {
+        return "good afternoon"
+    }
+    else if (hour >= 18 && hour <= 21) {
+        return "good evening"
+    }
+    else if (hour >= 22 && hour <= 24) {
+        return "good night"
+    }
 
 }
 
 //4. Write a function that will take in a number and return 'fever' if it indicates a fever (over 98.6) and additionally if the person should go to the hospital (at or above 103) 'fever go to hospital' (hint: try this with string concatenation), if it is under return 'no fever'
 function isFever(temp) {
-
+    if (temp <= 98.6) {
+        return 'no fever'
+    }
+    else if (temp > 98.6 && temp < 103) {
+        return 'fever'
+    }
+    else if (temp >= 103) {
+        return 'fever go to hospital'
+    }
 }
 
 //5. Write a function that takes in a car object, if it is not moving then return true
@@ -47,7 +87,12 @@ let myCar = {
 }
 
 function isStopped(car) {
-
+    if (car.moving === false) {
+        return true
+    }
+    else {
+        return false
+    }
 }
 
 //6. Write a function that returns true if a dish is yours and is dirty, or false if one of the statements is false
@@ -58,5 +103,12 @@ let dish = {
 }
 
 function washDish(dish) {
+    if (dish.yourDish === true && dish.isDirty === true) {
+        return true
+    }
+    else {
+        return false
+    }
+
 
 }
